92e7880055f1bcd0fd804c2581b618e254a6b1d8
nabil <abiel769@gmail.com>
2023-05-12 18:52:53 +0800

    alhamdulillah disyukuri :)

--- SmartParking/01_edge_smartparking/01_edge_smartparking.ino
+++ SmartParking/01_edge_smartparking/01_edge_smartparking.ino
@@ -1,56 +1,55 @@
+#include <SPI.h>
 #include <MFRC522.h>
-#include <MFRC522Extended.h>
-#include <deprecated.h>
-#include <require_cpp11.h>
-#include <ESP32Servo.h>
 
-//  Input
-#define BUTTON_GREEN 17
-#define BUTTON_RED 18
-#define RFID_SS 5
-#define RFID_RST 22
-#define PROXIMITY_PIN 34
-
-//  Output
-#define LED_RED 32
-#define LED_BLUE 33
-#define SERVO_PIN 23
-
-// Inisialisasi
-Servo gateServo;
-
-// Variabel global
+#define SS_PIN 5
+#define RST_PIN 22
+ 
+MFRC522 rfid(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key; 
+byte nuidPICC[4];
 String cardUID;
-String cardOwnerName;
-String cardStatus;
-MFRC522 mfrc522(RFID_SS, RFID_RST);
-String readRFID() {
-  cardUID = "";
-  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-    for (byte i = 0; i < mfrc522.uid.size; i++) {
-      cardUID += String(mfrc522.uid.uidByte[i], HEX);
-    }
+
+void setup() { 
+  Serial.begin(9600);
+  SPI.begin(); // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522 
+
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
   }
-  return cardUID;
+}
+ 
+void loop() {
+  readRFID();
+  if (cardUID != "") 
+    {
+      Serial.println(cardUID); 
+      if (cardUID == "90a44c26") Serial.println("Oke,lanjut");
+      cardUID="";
+    }
 }
 
-void setup()
+void readRFID()
   {
-    Serial.begin(115200);
-    // PB
-    pinMode(BUTTON_GREEN, INPUT_PULLUP);
-    pinMode(BUTTON_RED, INPUT_PULLUP);
-    // RFID
-    SPI.begin();
-    mfrc522.PCD_Init();
-    // Servo
-    setupServo();
-    // Proximity
-    pinMode(PROXIMITY_PIN, INPUT);
-  }
+    if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+  if ( ! rfid.PICC_ReadCardSerial())
+    return;
 
-void loop()
-  {
-    cardUID = readRFID();
-    //testServo();
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
+    return;
+  }
+  for (byte i = 0; i < 4; i++) {
+    nuidPICC[i] = rfid.uid.uidByte[i];
+  }
+  cardUID = "";
+  for (byte i = 0; i < 4; i++) {
+    cardUID += (nuidPICC[i] < 0x10 ? "0" : "") + String(nuidPICC[i], HEX);
+  }
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
   }
--- SmartParking/01_edge_smartparking/aktuator.ino
+++ SmartParking/01_edge_smartparking/aktuator.ino
@@ -1,14 +1,14 @@
-void setupServo()
-  {
-    gateServo.setPeriodHertz(50);             // standard 50 hz servo
-    gateServo.attach(SERVO_PIN, 500, 2400);   // attaches the servo on pin 2 to the servo object
-                                            // and set min and max pulse widths
-  }
+// void setupServo()
+//   {
+//     gateServo.setPeriodHertz(50);             // standard 50 hz servo
+//     gateServo.attach(SERVO_PIN, 500, 2400);   // attaches the servo on pin 2 to the servo object
+//                                             // and set min and max pulse widths
+//   }
 
-void testServo()
-  {
-    gateServo.write(90);  // tell servo to go to position '90'
-    delay(1000);        // wait 1s
-    gateServo.write(0);   // tell servo to go to position '0'
-    delay(1000);        // wait 1s
-  }
+// void testServo()
+//   {
+//     gateServo.write(90);  // tell servo to go to position '90'
+//     delay(1000);        // wait 1s
+//     gateServo.write(0);   // tell servo to go to position '0'
+//     delay(1000);        // wait 1s
+//   }
