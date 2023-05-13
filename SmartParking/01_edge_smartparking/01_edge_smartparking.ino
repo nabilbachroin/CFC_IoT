0bb0e89073980667c1d4d27731395f66b18f55be
nabil <abiel769@gmail.com>
2023-05-13 13:48:34 +0800

    adding servo movement

--- SmartParking/01_edge_smartparking/01_edge_smartparking.ino
+++ SmartParking/01_edge_smartparking/01_edge_smartparking.ino
@@ -1,12 +1,15 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <ESP32Servo.h>
 
 #define SS_PIN 5
 #define RST_PIN 22
-const int ledBluePin = 32;
-const int ledRedPin = 33;
+#define servoPin 25
+const int ledBluePin = 33;
+const int ledRedPin = 32;
  
 MFRC522 rfid(SS_PIN, RST_PIN);
+Servo gateServo;
 MFRC522::MIFARE_Key key; 
 byte nuidPICC[4];
 String cardUID;
@@ -15,6 +18,8 @@ void setup() {
   Serial.begin(9600);
   SPI.begin();
   rfid.PCD_Init();
+  gateServo.setPeriodHertz(50); 
+  gateServo.attach(servoPin, 500, 2400); // minim and maks (ms)
   pinMode(ledBluePin, OUTPUT);
   pinMode(ledRedPin, OUTPUT);
   digitalWrite(ledRedPin, LOW);
@@ -23,6 +28,7 @@ void setup() {
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
+  gateServo.write(180);
 }
  
 void loop() {
@@ -35,6 +41,7 @@ void loop() {
         {
           Serial.println("Oke,lanjut");
           digitalWrite(ledBluePin, 1);
+          openGate();
         }
       else digitalWrite(ledRedPin, 1);
       delay(1000);
--- SmartParking/01_edge_smartparking/aktuator.ino
+++ SmartParking/01_edge_smartparking/aktuator.ino
@@ -12,3 +12,16 @@
 //     gateServo.write(0);   // tell servo to go to position '0'
 //     delay(1000);        // wait 1s
 //   }
+
+void openGate()
+  {
+    for (int pos = 180; pos >= 90; pos -= 1) {
+    gateServo.write(pos);
+    delay(15);}
+    delay(2000);
+    // Disini baca proximity
+    for (int pos = 90; pos <= 180; pos += 1) {
+    gateServo.write(pos);
+    delay(33);
+  }
+  }
